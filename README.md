983d329f7657bd043f57f5573470bb4775173907
Hleb Yarmolchyk <hleb_yarmolchyk@epam.com>
2024-02-18 21:55:41 +0100

    FEAT: add scripts

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
-# LambdaTerminal
-Lambda terminal itself
+# AWS Lambda Terminal
+
+This code allows you to execute custom commands via AWS Lambda. It runs the command and logs the output to a temporary file. The logs can then be viewed in a browser-based terminal interface.
+
+
+## How does it work?
+
+The script uses the `executeCommand` function to execute a received command and write the output (& error messages) to a temporary file. The directory of the executed command is also stored; making it possible to execute context-based commands like `cd ..`.
+
+Meanwhile, the `generateResponses` function generates a HTML template which presents an interface similar to a terminal. This allows users the ability to view the execution log of the previously executed command.
+
+Finally, the `handler` function handles incoming event requests and extracts the IP address and command from the request. The function then executes the mentioned command and generates the HTML response which is further returned as a HTTP response.
+
+
+## Usage
+
+1. Deploy this script to AWS Lambda. Ensure it is set to run in a Amazon Linux Runtime. 
+2. The `handler` function should be specified as your Lambda function's handler in the AWS Lambda console.
+
+Remember to replace `YourLambdaFunctionName` with your own Lambda function name.
+
+
+## Notes
+
+- This script creates a unique temporary file per IP address. This means that different users (different IP addresses) will have different execution contexts (commands are executed independently).
+
+- Ensure that your AWS Lambda function has the proper permissions to execute the commands and write to temporary files. 
+
+- Be aware of the security implications of this script. It executes arbitrary commands received from an HTTP request. Therefore, make sure appropriate security measures are in place to ensure only trusted users can execute commands.
+
+
+## How to Secure
+
+Here are some security recommendations in order to help protect your function:
+
+- Use AWS IAM roles and policies to control permissions of your Lambda function.
+
+- Limit network access by using security groups and network ACLs.
+
+- Enable AWS Shield to protect your application from DDoS attacks.
+
+- Monitor function invocations with AWS CloudTrail.
+  
+Remember, managing the security of your function is a shared responsibility between AWS and you.
---
+++ hello.sh
@@ -0,0 +1,87 @@
+executeCommand() {
+    local command=$1
+    local identifier=$2
+
+    [[ -f /tmp/directory_${identifier} ]] && cd $(cat /tmp/directory_${identifier}) || true
+
+    if [ "$command" = "clear" ]; then
+        >/tmp/logger_${identifier}
+        return
+    fi
+
+    {
+        eval "$command" || true
+    } 2>>/tmp/logger_${identifier} >>/tmp/logger_${identifier}
+
+    echo "$(pwd)" >/tmp/directory_${identifier}
+}
+
+generateResponses() {
+    local identity=$1
+
+    {
+        echo "
+            <html>
+                <body style='background-color: #000; color: #FFF; font-family: Courier New, Courier, monospace;'>
+                    <style>
+                        form {
+                            background-color: #000;
+                            color: #FFF;
+                            padding: 1px;
+                            font-family: 'Courier New', Courier, monospace;
+                        }
+                        pre {
+                            background-color: #000;
+                            color: #FFF;
+                            padding: 0px;
+                            white-space: pre-wrap;
+                            white-space: -moz-pre-wrap;
+                            white-space: -pre-wrap;
+                            white-space: -o-pre-wrap;
+                            word-wrap: break-word;
+                        }
+                        input[type='text']:focus {
+                            outline: none;
+                        }
+                    </style>
+                    <pre>"
+
+        [[ -f /tmp/logger_${identity} ]] && cat /tmp/logger_${identity}
+
+        echo "
+                    </pre>
+                </body>
+            </html>
+        "
+    } >/tmp/template.html
+
+    {
+        echo "
+            <form method='GET' action=''>
+                <label>$(pwd)$ </label>
+                <input id='prompt' name='command' type='text' size='100' style='background-color: #000; color: #FFF; border: none;'>
+                <input type='submit' style='visibility: hidden;'>
+            </form>
+            <script>
+                window.onload = function() {
+                    window.scrollTo(0,document.body.scrollHeight);
+                    document.getElementById('prompt').focus();
+                }
+            </script> 
+        "
+    } >>/tmp/template.html
+}
+
+handler() {
+    local event=$1
+
+    local ipAddress=$(echo "$event" | jq -r '.headers."x-forwarded-for"')
+    local parsedCommand=$(echo "$event" | jq -r '.queryStringParameters.command // ""')
+
+    executeCommand "$parsedCommand" "$ipAddress"
+    generateResponses "$ipAddress"
+
+    local htmlResponse=$(cat /tmp/template.html)
+
+    echo -e "{ \"isBase64Encoded\": true, \"statusCode\": 200, \"headers\": { \"Content-Type\": \"text/html\" }, \"body\": \"$(echo "$htmlResponse" | base64 | tr -d '\n')\" }"
+}
